9b3c902c11090a9998ea7e417ea00c622af8a335
Nagisa Sugishita <nagisa.sugishita@hotmail.co.jp>
2023-04-04 21:17:37 +0100

    Add removal hooks

--- cpsdppy/mip_solvers/base.py
+++ cpsdppy/mip_solvers/base.py
@@ -2,7 +2,6 @@
 
 """Base class of LP Solvers"""
 
-import indexremove
 import numpy as np
 
 
@@ -15,9 +14,46 @@ class BaseSolverInterface:
         self.solve_prehooks = []
         self.solve_posthooks = []
         self.solve_exit_hooks = []
+        self.remove_variables_hooks = []
+        self.remove_linear_constraints_hooks = []
+        self.remove_quadratic_constraints_hooks = []
         self.variable_index_lists = []
         self.linear_constraint_index_lists = []
         self.quadratic_constraint_index_lists = []
+
+    def add_hooks(self, hooks):
+        try:
+            self.remove_variables_hooks.append(hooks.remove_variables_hook)
+        except AttributeError:
+            pass
+        try:
+            self.remove_linear_constraints_hooks.append(
+                hooks.remove_linear_constraints_hook
+            )
+        except AttributeError:
+            pass
+        try:
+            self.remove_quadratic_constraints_hooks.append(
+                hooks.remove_quadratic_constraints_hook
+            )
+        except AttributeError:
+            pass
+        try:
+            self.solve_enter_hooks.append(hooks.solve_enter_hook)
+        except AttributeError:
+            pass
+        try:
+            self.solve_prehooks.append(hooks.solve_prehook)
+        except AttributeError:
+            pass
+        try:
+            self.solve_posthooks.append(hooks.solve_posthook)
+        except AttributeError:
+            pass
+        try:
+            self.solve_exit_hooks.append(hooks.solve_exit_hook)
+        except AttributeError:
+            pass
 
     def remove_variables(self, index):
         index = np.asarray(index)
@@ -25,8 +61,8 @@ class BaseSolverInterface:
             np.testing.assert_equal(index.ndim, 1)
             index = np.nonzero(index)[0]
         self._remove_variables_impl(index)
-        for lst in self.variable_index_lists:
-            indexremove.remove(lst.ravel(), index.ravel())
+        for hook in self.remove_variables_hooks:
+            hook(self, index)
 
     def remove_linear_constraints(self, index):
         index = np.asarray(index)
@@ -34,8 +70,8 @@ class BaseSolverInterface:
             np.testing.assert_equal(index.ndim, 1)
             index = np.nonzero(index)[0]
         self._remove_linear_constraints_impl(index)
-        for lst in self.linear_constraint_index_lists:
-            indexremove.remove(lst.ravel(), index.ravel())
+        for hook in self.remove_linear_constraints_hooks:
+            hook(self, index)
 
     def remove_quadratic_constraints(self, index):
         index = np.asarray(index)
@@ -43,8 +79,8 @@ class BaseSolverInterface:
             np.testing.assert_equal(index.ndim, 1)
             index = np.nonzero(index)[0]
         self._remove_quadratic_constraints_impl(index)
-        for lst in self.quadratic_constraint_index_lists:
-            indexremove.remove(lst.ravel(), index.ravel())
+        for hook in self.remove_quadratic_constraints_hooks:
+            hook(self, index)
 
     def solve(self):
         """Solve the problem and call hooks"""
