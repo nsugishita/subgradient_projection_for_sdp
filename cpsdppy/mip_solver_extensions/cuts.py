5b730d61871a3f45436e0402a671eef5c9bc3689
Nagisa Sugishita <nagisa.sugishita@hotmail.co.jp>
2023-04-12 10:44:21 +0100

    Improve efficiency of LMICuts.add_lmi_cuts

--- cpsdppy/mip_solver_extensions/cuts.py
+++ cpsdppy/mip_solver_extensions/cuts.py
@@ -230,74 +230,70 @@ class LMICuts:
         coef[i] x - offset[i] : PSDCone
         ```
         """
-        if isinstance(coef, np.ndarray) and coef.ndim == 2:
-            coef = coef[None]
-        if isinstance(coef, scipy.sparse.spmatrix):
+        if isinstance(coef, (list, tuple)):
+            if len(coef) == 0:
+                return
+            if isinstance(coef[0], np.ndarray):
+                coef = np.stack(coef)
+        elif isinstance(coef, scipy.sparse.spmatrix):
             coef = [coef]
-        offset = np.atleast_2d(offset)
-        n_new_cuts = len(coef)
+        # coef is a numpy array or a list of sparse matrices
+        if isinstance(coef, np.ndarray):
+            coef = coef.reshape(-1, self.n_variables)
+            coef = scipy.sparse.csr_array(coef)
+        else:
+            coef = scipy.sparse.vstack(coef)
+        # coef is a sparse matrix of shape (3 * n_new_cuts, n_variables)
+        n_new_cuts = coef.shape[0] // 3
+        offset = offset.reshape(n_new_cuts, 3)
+        np.testing.assert_equal(coef.shape, (3 * n_new_cuts, self.n_variables))
         self.n += n_new_cuts
         model = self.model()
+        n = model.get_n_variables()
+        Z = scipy.sparse.csr_array(
+            ([], ([], [])), shape=(3 * n_new_cuts, n - coef.shape[1])
+        )
+        (
+            new_variable_index,
+            new_quadratic_constraint_index,
+        ) = model.add_2x2_psd_variables(n_new_cuts)
+        D = -scipy.sparse.eye(3 * n_new_cuts)
+        constr_coef = scipy.sparse.hstack([coef, Z, D])
+        new_linear_constraint_index = model.add_linear_constraints(
+            shape=3 * n_new_cuts,
+            coef=constr_coef,
+            sense="E",
+            rhs=offset.ravel(),
+        )
+        new_linear_constraint_index = new_linear_constraint_index.reshape(
+            n_new_cuts, 3
+        )
 
-        for _coef, _offset in zip(coef, offset):
-            n = model.get_n_variables()
-            Z = scipy.sparse.csr_array(
-                ([], ([], [])), shape=(3, n - _coef.shape[1])
-            )
-            (
+        self.coef = scipy.sparse.vstack([self.coef, coef]).tocsr()
+
+        self.offset = np.concatenate([self.offset, offset], axis=0)
+
+        self.variable_index = np.concatenate(
+            [
+                self.variable_index,
                 new_variable_index,
+            ],
+            axis=0,
+        )
+        self.linear_constraint_index = np.concatenate(
+            [
+                self.linear_constraint_index,
+                new_linear_constraint_index,
+            ],
+            axis=0,
+        )
+        self.quadratic_constraint_index = np.concatenate(
+            [
+                self.quadratic_constraint_index,
                 new_quadratic_constraint_index,
-            ) = model.add_2x2_psd_variables()
-            D = np.array(
-                [
-                    [
-                        -1,
-                        0,
-                        0,
-                    ],
-                    [
-                        0,
-                        -1,
-                        0,
-                    ],
-                    [
-                        0,
-                        0,
-                        -1,
-                    ],
-                ]
-            )
-            coef = scipy.sparse.hstack([_coef, Z, D])
-            new_linear_constraint_index = model.add_linear_constraints(
-                shape=3, coef=coef, sense="E", rhs=_offset
-            )
+            ]
+        )
 
-            self.coef = scipy.sparse.vstack([self.coef, _coef]).tocsr()
-
-            self.offset = np.concatenate(
-                [self.offset, _offset[None, :]], axis=0
-            )
-
-            self.variable_index = np.concatenate(
-                [
-                    self.variable_index,
-                    new_variable_index,
-                ],
-                axis=0,
-            )
-            self.linear_constraint_index = np.concatenate(
-                [
-                    self.linear_constraint_index,
-                    new_linear_constraint_index[None],
-                ],
-                axis=0,
-            )
-            self.quadratic_constraint_index = np.concatenate(
-                [
-                    self.quadratic_constraint_index,
-                    new_quadratic_constraint_index,
-                ]
-            )
         self.last_active_iteration = np.concatenate(
             [
                 self.last_active_iteration,
