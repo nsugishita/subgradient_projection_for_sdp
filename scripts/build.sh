7a6aabb493bdb8e1c3b9bc6aed1f48766cd0a9af
Nagisa Sugishita <nagisa.sugishita@hotmail.co.jp>
2023-04-18 11:55:33 +0100

    Update path to the compiler and cmake

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,7 +10,7 @@ pushd extensions/indexremove/
 rm -rf build
 mkdir build
 cd build
-CXX=~/local/bin/g++ ~/local/bin/cmake .. -DCMAKE_INSTALL_PREFIX="$PREFIX"
+CXX=~/local/default/bin/g++ ~/local/default/bin/cmake ..
 make
 popd
 
