67917ffee2c1aeaacd76550688efda2ef7abde72
Nagisa Sugishita <nagisa.sugishita@hotmail.co.jp>
2023-04-23 11:57:50 +0100

    Build uniquelist

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -5,6 +5,7 @@
 # . scripts/init.sh
 
 export PYTHONPATH="$PYTHONPATH":"$(pwd)/extensions/indexremove/build"
+export PYTHONPATH="$PYTHONPATH":"$(pwd)/extensions/uniquelist/build"
 export OMP_NUM_THREADS=1
 
 . ./env/bin/activate
--- scripts/build.sh
+++ scripts/build.sh
@@ -3,16 +3,24 @@
 # Build extensions. Call this script from the top directory of the project.
 # . ./scripts/build.sh
 
+set -e
+
 . ./scripts/activate.sh
 
 pushd extensions/indexremove/
-
-rm -rf build
-mkdir build
+mkdir -p build
 cd build
 CXX=~/local/default/bin/g++ ~/local/default/bin/cmake ..
 make
 popd
-
 python -c "import indexremove"
-echo "built extensions successfully"
+echo "built 'indexremove' successfully"
+
+pushd extensions/uniquelist/
+mkdir -p build
+cd build
+CXX=~/local/default/bin/g++ ~/local/default/bin/cmake ..
+make
+popd
+python -c "import uniquelist"
+echo "built 'uniquelist' successfully"
