0f544279e81a4b8d8dfc9e5ae6f580607e3bb186
Nagisa Sugishita <nagisa.sugishita@hotmail.co.jp>
2023-07-11 18:00:51 +0100

    Import mosek_solver

--- cpsdppy/sdp_solvers/__init__.py
+++ cpsdppy/sdp_solvers/__init__.py
@@ -3,4 +3,5 @@
 """SDP solvers"""
 
 from cpsdppy.sdp_solvers import cutting_plane  # noqa: F401
+from cpsdppy.sdp_solvers import mosek_solver  # noqa: F401
 from cpsdppy.sdp_solvers import subgradient_projection  # noqa: F401
