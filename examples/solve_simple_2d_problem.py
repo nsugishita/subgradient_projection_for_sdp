246913fa11755c98435481a3b60da807c94e47de
Nagisa Sugishita <nagisa.sugishita@hotmail.co.jp>
2023-08-22 23:27:00 +0100

    Remove unused setup and problem in example

--- examples/solve_simple_2d_problem.py
+++ examples/solve_simple_2d_problem.py
@@ -1,79 +1,7 @@
 # -*- coding: utf-8 -*-
 
-"""Optimization of a linear function over a circle using SDP
+"""Run the subgradient projection method on a simple problem"""
 
-In this example we optimize a linear function over a circle.
-
-min  c^T x
-s.t. |x|^2 <= 1.
-
-Denote x = [p q].
-The circle is represented as a linear matrix inequality
-
-min  c^T [p q]
-
-     [1+p   q ]
-s.t. [        ]  :  PSD
-     [ q   1-p]
-
-More specifically, we use the minimum eigenvalue to formulate the constraint
-
-min  c^T [p q]
-
-                 [1+p   q ]
-s.t. lambda_min  [        ]  >=  0
-                 [ q   1-p]
-
-Let us denote
-
-    [1+p   q ]
-S = [        ]
-    [ q   1-p]
-
-     [ 1   0 ]     [ 0   1 ]     [ 1   0 ]
-   = [       ] p + [       ] q + [       ].
-     [ 0  -1 ]     [ 1   0 ]     [ 0   1 ]
-
-
-   = A p + B q + C.
-
-We note that lambda_min is a concave function.
-The subgradient g of lambda_min at bar_x = [bar_p bar_q] is given by
-
-g_p = -v_min(bar_S) v_min(bar_S)^T bullet A
-    = -v_min(bar_S) A v_min(bar_S)^T,
-g_q = -v_min(bar_S) v_min(bar_S)^T bullet B
-    = -v_min(bar_S) B v_min(bar_S)^T,
-
-where v_min(bar_S) is a eigenvector associated with the minimum eigenvalue
-lambda_min(bar_S) of bar_S = A bar_p + B bar_q + C.
-The liniearised constraint is
-
-lambda_min(bar_S) + g^T (x - bar_x) >= 0
-
-We have
-
-g^T bar_x + v_min(bar_S)^T C v_min(bar_S)
-= v_min(bar_S)^T (A bar_p + B bar_q + C) v_min(bar_S)
-= lambda_min(bar_S)
-
-Therefore, the linearised constraint can be written as
-
-v_min(bar_S)^T S v_min(bar_S)^T >= 0
-
-Thus, we replaces the positive definiteness constraint with
-
-v^T S v >= 0.
-
-Similarly, we can replace the positive definiteness constraint with
-
-V^T S V >= 0
-
-with V in R^{n x 2}.
-This constraint can be written as SOCP.
-"""
-
-import logging
 import os
 
 import matplotlib.pyplot as plt
@@ -82,63 +10,29 @@ import scipy.sparse
 
 import cpsdppy
 
-# import shutil
-
 plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
 
+plot_dir = "outputs/simple_example/v1"
 
-logger = logging.getLogger(__name__)
-
-plot_dir = "tmp/circle_sdp/v2"
-
-# if os.path.exists(plot_dir):
-#     shutil.rmtree(plot_dir)
 os.makedirs(plot_dir, exist_ok=True)
 
-grid_size = 30
-
-setup = 3
+grid_size = 300
 
 
 def main():
-    problem_name = "e"
-    cpsdppy.logging_helper.setup()
     config = cpsdppy.config.Config()
 
-    if setup == 1:
-        config.eval_lb_every = 0
-        config.iteration_limit = 3
-        config.step_size = 5.0
-        config.tol = 1e-6
-        config.feas_tol = 1e-6
-        config.add_cuts_after_optimality_step = 0
-        config.n_linear_cuts = 1
-        config.eigen_comb_cut = 1
-        initial_x = [-4, 2]
-    elif setup == 2:
-        config.eval_lb_every = 0
-        config.iteration_limit = 3
-        config.step_size = 2.0
-        config.tol = 1e-6
-        config.feas_tol = 1e-6
-        config.add_cuts_after_optimality_step = 0
-        config.n_linear_cuts = 1
-        config.eigen_comb_cut = 0
-        initial_x = [0, -1]
-    elif setup == 3:
-        config.eval_lb_every = 0
-        config.iteration_limit = 3
-        config.step_size = 2.0
-        config.tol = 1e-6
-        config.feas_tol = 1e-6
-        config.add_cuts_after_optimality_step = 0
-        config.n_linear_cuts = 1
-        config.eigen_comb_cut = 0
-        initial_x = [-3, -3]
-    else:
-        raise ValueError
+    config.eval_lb_every = 0
+    config.iteration_limit = 3
+    config.step_size = 2.0
+    config.tol = 1e-6
+    config.feas_tol = 1e-6
+    config.add_cuts_after_optimality_step = 0
+    config.n_linear_cuts = 1
+    config.eigen_comb_cut = 0
+    initial_x = [-3, -3]
 
-    problem_data = get_problem_data(problem_name)
+    problem_data = get_problem_data()
     problem_data["target_objective"] = cpsdppy.sdp_solvers.mosek_solver.run(
         problem_data, config
     )["primal_objective"]
@@ -160,21 +54,10 @@ def main():
     fig, ax = plt.subplots()
     ax.axis("equal")
     box_color = "none"
-    if setup == 1:
-        ax.axhline(-3, lw=1, color=box_color)
-        ax.axhline(3, lw=1, color=box_color)
-        ax.axvline(-3, lw=1, color=box_color)
-        ax.axvline(3, lw=1, color=box_color)
-    elif setup == 2:
-        ax.axhline(-3, lw=1, color=box_color)
-        ax.axhline(3, lw=1, color=box_color)
-        ax.axvline(-3, lw=1, color=box_color)
-        ax.axvline(3, lw=1, color=box_color)
-    elif setup == 3:
-        ax.axhline(-5, lw=1, color=box_color)
-        ax.axhline(4, lw=1, color=box_color)
-        ax.axvline(-4, lw=1, color=box_color)
-        ax.axvline(4, lw=1, color=box_color)
+    ax.axhline(-5, lw=1, color=box_color)
+    ax.axhline(4, lw=1, color=box_color)
+    ax.axvline(-4, lw=1, color=box_color)
+    ax.axvline(4, lw=1, color=box_color)
 
     xlim = ax.get_xlim()
     ylim = ax.get_ylim()
@@ -195,7 +78,7 @@ def main():
     )
 
     os.makedirs(plot_dir, exist_ok=True)
-    figpath = f"{plot_dir}/sdp_{problem_name}.pdf"
+    figpath = f"{plot_dir}/feasible_set.pdf"
     # default: 6.4, 4.8
     fig.set_size_inches(3.2, 2.4)
     fig.savefig(figpath, dpi=300, transparent=True)
@@ -268,66 +151,19 @@ def main():
 
     # ax.legend(loc="upper center")
 
-    if setup == 1:
-        x_offsets = [
-            [0.0, 0.4],
-            [-0.4, 0.0],
-            [0.1, 0.4],
-            [-0.1, 0.4],
-        ]
-        x_box = [False, False, False, False]
-        v_offsets = [
-            [0.4, 0.0],
-            [0.4, 0.15],
-            [0.15, -0.3],
-        ]
-        v_box = [False, False, True]
-    elif setup == 2:
-        x_offsets = [
-            [0.0, 0.4],
-            [-0.4, 0.0],
-            [0.1, 0.4],
-            [-0.1, 0.4],
-        ]
-        x_box = [False, False, False, False]
-        v_offsets = [
-            [0.4, 0.0],
-            [0.4, 0.15],
-            [0.15, -0.3],
-        ]
-        v_box = [False, False, True]
-    elif setup == 3:
-        # With subscripts
-        # x_offsets = [
-        #     [0.0, 0.6],
-        #     [-0.6, 0.6],
-        #     [0.8, -0.2],
-        #     [0.8, 0.0],
-        # ]
-        # x_box = [False, False, False, False]
-        # v_offsets = [
-        #     [-0.4, -0.4],
-        #     [0.5, -0.5],
-        #     [0.8, 0.1],
-        # ]
-        # v_box = [False, False, True]
-
-        # With superscripts
-        x_offsets = [
-            [-0.2, 0.4],
-            [-0.7, 0.6],
-            [1.1, -0.1],
-            [1.1, 0.0],
-        ]
-        x_box = [False, False, False, False]
-        v_offsets = [
-            [-0.5, -0.8],
-            [0.7, -0.5],
-            [1.1, 0.2],
-        ]
-        v_box = [False, False, True]
-    else:
-        raise ValueError
+    x_offsets = [
+        [-0.2, 0.4],
+        [-0.7, 0.6],
+        [1.1, -0.1],
+        [1.1, 0.0],
+    ]
+    x_box = [False, False, False, False]
+    v_offsets = [
+        [-0.5, -0.8],
+        [0.7, -0.5],
+        [1.1, 0.2],
+    ]
+    v_box = [False, False, True]
     bbox = dict(boxstyle="square,pad=0.00", ec="none", fc="white", alpha=0.8)
 
     for i in range(len(x_list)):
@@ -353,142 +189,26 @@ def main():
     ax.set_yticks([])
 
     os.makedirs(plot_dir, exist_ok=True)
-    figpath = f"{plot_dir}/sdp_linear_cut_{problem_name}.pdf"
+    figpath = f"{plot_dir}/iterates.pdf"
     fig.savefig(figpath, dpi=300, transparent=True)
     fig.savefig(figpath.replace("pdf", "png"), dpi=300)
     print(figpath)
 
 
-def get_problem_data(problem_name):
-    if problem_name == "a":
-        objective_coef = np.array([0.0, 1.0])
+def get_problem_data():
+    objective_coef = np.array([0.0, 1.0])
+    rng = np.random.RandomState(1)
+    n = 3
+    a = rng.normal(size=(n, n)) / n
+    for i in range(n):
+        for j in range(i, n):
+            a[i, j] = a[j, i]
+    b = rng.normal(size=(n, n)) / n
+    for i in range(n):
+        for j in range(i, n):
+            b[i, j] = b[j, i]
+    c = -np.eye(n)
 
-        a = np.array([[1, 0], [0, -1]], dtype=float)
-        b = np.array([[0, 1], [1, 0]], dtype=float)
-        c = -np.array([[1, 0], [0, 1]], dtype=float)
-
-    elif problem_name == "b":
-        objective_coef = np.array([0.0, 1.0])
-
-        a = np.array(
-            [
-                [0, 1, 0],
-                [1, 0, 1],
-                [0, 1, 0],
-            ],
-            dtype=float,
-        )
-        b = np.array(
-            [
-                [0, 0, 0.8],
-                [0, 0, 0],
-                [0.8, 0, 0],
-            ],
-            dtype=float,
-        )
-        b = np.array(
-            [
-                [0, 0, 1],
-                [0, 0, 0],
-                [1, 0, 0],
-            ],
-            dtype=float,
-        )
-        c = -np.array(
-            [
-                [1, 0, 0],
-                [0, 1, 0],
-                [0, 0, 1],
-            ],
-            dtype=float,
-        )
-
-    elif problem_name == "c":
-        objective_coef = np.array([0, 1.0])
-        a = np.array(
-            [
-                [0, 1, 0, -1],
-                [1, 0, 1, 0],
-                [0, 1, 0, 1],
-                [-1, 0, 1, 0],
-            ],
-            dtype=float,
-        )
-        b = np.array(
-            [
-                [0, 0, -1, 0],
-                [0, 0, 1, -1],
-                [-1, 1, 0, 0],
-                [0, -1, 0, 0],
-            ],
-            dtype=float,
-        )
-        c = -np.array(
-            [
-                [1, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 0, 1, 0],
-                [0, 0, 0, 1],
-            ],
-            dtype=float,
-        )
-
-    elif problem_name == "d":
-        objective_coef = np.array([0.0, 1.0])
-        a = np.array(
-            [
-                [0, 0, 1, 0],
-                [0, 0, -0.5, 1],
-                [1, -0.5, 0, 0],
-                [0, 1, 0, 0],
-            ],
-            dtype=float,
-        )
-        b = np.array(
-            [
-                [0, 0.5, 0, -1],
-                [0.5, 0, 0.5, 0],
-                [0, 0.5, 0, 0.5],
-                [-1, 0, 0.5, 0],
-            ],
-            dtype=float,
-        )
-        c = -np.array(
-            [
-                [1, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 0, 1, 0],
-                [0, 0, 0, 1],
-            ],
-            dtype=float,
-        )
-
-    elif problem_name == "e":
-        objective_coef = np.array([0.0, 1.0])
-        if setup == 1:
-            rng = np.random.RandomState(0)
-            n = 10
-        elif setup == 2:
-            rng = np.random.RandomState(0)
-            n = 3
-        elif setup == 3:
-            rng = np.random.RandomState(1)
-            n = 3
-        else:
-            raise ValueError
-        # a = rng.normal(size=(n, n)) / np.sqrt(n)
-        a = rng.normal(size=(n, n)) / n
-        for i in range(n):
-            for j in range(i, n):
-                a[i, j] = a[j, i]
-        # b = rng.normal(size=(n, n)) / np.sqrt(n)
-        b = rng.normal(size=(n, n)) / n
-        for i in range(n):
-            for j in range(i, n):
-                b[i, j] = b[j, i]
-        c = -np.eye(n)
-
-    # constr_coefs = np.stack([a, b])
     constr_coefs = [scipy.sparse.csr_matrix(a), scipy.sparse.csr_matrix(b)]
     constr_offset = scipy.sparse.csr_matrix(c)
 
