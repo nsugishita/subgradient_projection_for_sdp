109d963fceb194273012c1e9d960f51e20bb6eb4
Nagisa Sugishita <nagisa.sugishita@hotmail.co.jp>
2024-01-25 18:11:22 -0500

    Save the time of each iteration

--- examples/run_cosmo_for_5_mins.jl
+++ examples/run_cosmo_for_5_mins.jl
@@ -1,4 +1,143 @@
-using FileIO, COSMO, SparseArrays, LinearAlgebra, Test, JuMP, MosekTools, NPZ
+using FileIO, COSMO, SparseArrays, LinearAlgebra, Test, JuMP, MosekTools, Printf, NPZ
+
+function optimize_with_iteration_time!(ws::COSMO.Workspace{T}) where {T <: AbstractFloat}
+    # >>>
+    out = Dict(
+        "walltime" => [],
+    )
+    # <<<
+
+    !ws.states.IS_ASSEMBLED && throw(ErrorException("The model has to be assembled! / set! before optimize!() can be called."))
+
+    # start timer
+    solver_time_start = time()
+
+    settings = ws.settings
+
+    # perform chordal decomposition
+    if settings.decompose
+        if !ws.states.IS_CHORDAL_DECOMPOSED
+            ws.times.graph_time = @elapsed COSMO.chordal_decomposition!(ws)
+        elseif ws.ci.decompose
+            COSMO.pre_allocate_variables!(ws)
+        end
+    end
+
+    # create scaling variables
+    # with scaling    -> uses mutable diagonal scaling matrices
+    # without scaling -> uses identity matrices
+    if !ws.states.IS_SCALED
+        ws.sm = (settings.scaling > 0) ? COSMO.ScaleMatrices{T}(ws.p.model_size[1], ws.p.model_size[2]) : COSMO.ScaleMatrices{T}()
+    end
+
+    # we measure times always in Float64
+    ws.times.factor_update_time = 0.
+    ws.times.proj_time  = 0. #reset projection time
+    ws.times.setup_time = @elapsed COSMO.setup!(ws);
+    if settings.verbose_timing
+        ws.times.update_time = 0.
+        ws.times.accelerate_time = 0.
+    end
+
+    # instantiate variables
+    status = :Undetermined
+    cost = T(Inf)
+    res_info = COSMO.ResultInfo(T(Inf), T(Inf), zero(T), zero(T), ws.rho_updates)
+    iter = 0
+    ws.safeguarding_iter = 0
+    # print information about settings to the screen
+    settings.verbose && COSMO.print_header(ws)
+    time_limit_start = time()
+
+    m, n = ws.p.model_size
+
+
+    COSMO.allocate_loop_variables!(ws, m, n)
+
+    # warm starting the operator variable
+    @. ws.vars.w[1:n] = ws.vars.x[1:n]
+    @. ws.vars.w[n+1:n+m] = one(T) / ws.ρvec * ws.vars.μ + ws.vars.s.data
+
+    # change state of the workspace
+    ws.states.IS_OPTIMIZED = true
+
+    iter_start = time()
+
+    # do one initialisation step to make ADMM iterates agree with standard ADMM
+    COSMO.admm_x!(ws.vars.s, ws.ν, ws.s_tl, ws.ls, ws.sol, ws.vars.w, ws.kkt_solver, ws.p.q, ws.p.b, ws.ρvec, settings.sigma, m, n)
+    COSMO.admm_w!(ws.vars.s, ws.x_tl, ws.s_tl, ws.vars.w, settings.alpha, m, n);
+
+    while iter + ws.safeguarding_iter < settings.max_iter
+        iter += 1
+
+        COSMO.acceleration_pre!(ws.accelerator, ws, iter)
+
+        if COSMO.update_suggested(ws.infeasibility_check_due, ws.accelerator)
+            COSMO.recover_μ!(ws.vars.μ, ws.vars.w_prev, ws.vars.s, ws.ρvec, n) # μ_k kept in sync with s_k, w already updated to w_{k+1}
+            @. ws.δy.data = ws.vars.μ
+        end
+
+        # ADMM steps
+        @. ws.vars.w_prev = ws.vars.w
+        ws.times.proj_time += COSMO.admm_z!(ws.vars.s, ws.vars.w, ws.p.C, n)
+        COSMO.apply_rho_adaptation_rules!(ws.ρvec, ws.rho_updates, settings, iter, iter_start, ws.times, ws, n)
+        COSMO.admm_x!(ws.vars.s, ws.ν, ws.s_tl, ws.ls, ws.sol, ws.vars.w, ws.kkt_solver, ws.p.q, ws.p.b, ws.ρvec,settings.sigma, m, n)
+        COSMO.admm_w!(ws.vars.s, ws.x_tl, ws.s_tl, ws.vars.w, settings.alpha, m, n);
+
+        COSMO.acceleration_post!(ws.accelerator, ws, iter)
+        # @show(ws.vars.w)
+
+        # >>>
+        push!(out["walltime"], time() - iter_start)
+        # <<<
+
+        # convergence / infeasibility / timelimit checks
+        cost, status, res_info = COSMO.check_termination!(ws, settings, iter, cost, status, res_info, time_limit_start, n)
+        if status != :Undetermined
+            break
+        end
+
+    end #END-ADMM-MAIN-LOOP
+
+    COSMO.recover_μ!(ws.vars.μ, ws.vars.w_prev, ws.vars.s, ws.ρvec, n)
+
+    ws.times.iter_time = (time() - iter_start)
+    settings.verbose_timing && (ws.times.post_time = time())
+
+    # calculate primal and dual residuals
+    if iter + ws.safeguarding_iter == settings.max_iter
+        res_info = COSMO.calculate_result_info!(ws)
+        status = :Max_iter_reached
+    end
+
+    # reverse scaling for scaled feasible cases
+    if settings.scaling != 0
+        COSMO.reverse_scaling!(ws)
+    end
+
+    #reverse chordal decomposition
+    if ws.ci.decompose
+         COSMO.reverse_decomposition!(ws, settings)
+        y = -ws.vars.μ
+     else
+        @. ws.utility_vars.vec_m = -ws.vars.μ
+        y = ws.utility_vars.vec_m
+    end
+
+    ws.times.solver_time = time() - solver_time_start
+    settings.verbose_timing && (ws.times.post_time = time() - ws.times.post_time)
+
+    # print solution to screen
+    total_iter = ws.safeguarding_iter + iter
+    settings.verbose && COSMO.print_result(status, total_iter, ws.safeguarding_iter, cost, ws.times.solver_time, ws.settings.safeguard)
+
+    # create result object
+    COSMO.free_memory!(ws)
+    # >>>
+    return COSMO.Result{T}(ws.vars.x, y, ws.vars.s.data, cost, total_iter, ws.safeguarding_iter, status, res_info, ws.times), out;
+    # <<<
+
+end
 
 if size(ARGS)[1] == 0
     println("usage: julia $(PROGRAM_FILE) PROBLEM_NAME ...");
@@ -34,7 +173,7 @@ function run_cosmo_with_time_limit(data, time_limit)
          eps_rel= 1e-8,
     )
     COSMO.assemble!(model_direct, spzeros(m, m), c, cs1, settings = settings);
-    res = COSMO.optimize!(model_direct);
+    res, out = optimize_with_iteration_time!(model_direct);
 
     if res.status == :Solved
         status_code = 0
@@ -52,7 +191,7 @@ function run_cosmo_with_time_limit(data, time_limit)
         status_code = 6
     end
 
-    return Dict(
+    buf = Dict(
         "x" => res.x,
         "f" => res.obj_val,
         "f_star" => obj_true,
@@ -65,6 +204,12 @@ function run_cosmo_with_time_limit(data, time_limit)
         "max_norm_dual" => res.info.max_norm_dual,
         "status" => status_code,
     );
+
+    for (key, value) in out
+        buf[key] = stack(value, dims=1)
+    end
+
+    return buf;
 end
 
 run_cosmo_with_time_limit(load("./data/SDPLIB/data/mcp100.jld2"), 3);
@@ -79,4 +224,4 @@ for problem_name in ARGS
 end
 
 
-# vimquickrun: julia --project=juliaenv examples/run_cosmo_for_5mins.jl mcp100
+# vimquickrun: julia --project=juliaenv examples/run_cosmo_and_evaluate_iteration.jl mcp250-1 mcp250-2
