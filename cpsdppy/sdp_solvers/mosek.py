81d89a7121a1e11a25e909e300bcf1147b08aff9
Nagisa Sugishita <nagisa.sugishita@hotmail.co.jp>
2024-01-28 11:55:50 -0500

    Improve formulation in Mosek

--- cpsdppy/sdp_solvers/mosek.py
+++ cpsdppy/sdp_solvers/mosek.py
@@ -14,86 +14,76 @@ def run(problem_data, config):
     c = np.asarray(problem_data["objective_coefficient"])
 
     with fusion.Model() as model:
-        t = model.variable("x", fusion.Domain.unbounded([c.size]))
+        block_sizes = [
+            problem_data["lmi_constraint_offset"][block_index].shape[0]
+            for block_index in range(n_blocks)
+        ]
 
+        X = [
+            model.variable(
+                "X", fusion.Domain.inPSDCone(block_sizes[block_index])
+            )
+            for block_index in range(n_blocks)
+        ]
+
+        constraints = []
+
+        for i in range(len(c)):
+            lhs = 0
+            for block_index in range(n_blocks):
+                F = problem_data["lmi_constraint_coefficient"][block_index][i].tocoo()
+                coef = fusion.Matrix.sparse(
+                    F.shape[0], F.shape[1], F.row, F.col, F.data)
+                lhs = fusion.Expr.add(lhs, fusion.Expr.dot(coef, X[block_index]))
+            constraints.append(model.constraint(lhs, fusion.Domain.equalsTo(c[i])))
+
+        obj = 0
         for block_index in range(n_blocks):
-            # sum_{j = 1}^m A_{ij} x_j - A_{i0} : PSD
-            coef = problem_data["lmi_constraint_coefficient"][block_index]
-            offset = problem_data["lmi_constraint_offset"][block_index]
-            np.testing.assert_equal(type(coef), list)
-            np.testing.assert_equal(len(coef), c.size)
-            if c.size > 0:
-                np.testing.assert_equal(type(coef[0]), scipy.sparse.csr_matrix)
-            np.testing.assert_equal(type(offset), scipy.sparse.csr_matrix)
-            matrices = []
-            matrices.append(
-                fusion.Expr.neg(fusion.Expr.constTerm(offset.todense()))
-            )
-
-            for j in range(len(coef)):
-                coefj_scipy = coef[j].tocoo()
-                coefj_fusion = fusion.Matrix.sparse(
-                    coefj_scipy.shape[0],
-                    coefj_scipy.shape[1],
-                    coefj_scipy.row,
-                    coefj_scipy.col,
-                    coefj_scipy.data,
-                )
-                _t = fusion.Expr.reshape(
-                    fusion.Expr.repeat(
-                        t.index([j]), np.prod(coef[j].shape), 0
-                    ),
-                    list(coef[j].shape),
-                )
-                matrices.append(fusion.Expr.mulElm(_t, coefj_fusion))
-            matrices = [
-                fusion.Expr.reshape(x, np.r_[1, x.getShape()])
-                for x in matrices
-            ]
-            matrices = fusion.Expr.stack(0, matrices)
-            model.constraint(
-                fusion.Expr.sum(matrices, 0), fusion.Domain.inPSDCone()
-            )
-
-        model.objective(fusion.ObjectiveSense.Minimize, fusion.Expr.dot(t, c))
+            F = problem_data["lmi_constraint_offset"][block_index].tocoo()
+            coef = fusion.Matrix.sparse(
+                F.shape[0], F.shape[1], F.row, F.col, F.data)
+            obj = fusion.Expr.add(obj, fusion.Expr.dot(coef, X[block_index]))
+        model.objective(fusion.ObjectiveSense.Maximize, obj)
 
         res = mosek_utils.solve(model, config)
+        # res["x"] = [x_.level() for x_ in X]
+        res["y"] = np.array([c_.dual() for c_ in constraints]).reshape(-1)
 
-        target_objective = problem_data.get("target_objective", None)
-        test = (
-            (target_objective is not None)
-            and np.isfinite(res["primal_objective"])
-            and np.isfinite(res["dual_objective"])
-        )
-        if test:
-            target_tol = 5e-2
-            test = True
-            try:
-                np.testing.assert_allclose(
-                    res["primal_objective"],
-                    target_objective,
-                    rtol=target_tol,
-                    atol=target_tol,
-                )
-            except AssertionError as e:
-                print(e)
-                test = False
-            try:
-                np.testing.assert_allclose(
-                    res["dual_objective"],
-                    target_objective,
-                    rtol=target_tol,
-                    atol=target_tol,
-                )
-            except AssertionError as e:
-                print(e)
-                test = False
-            if not test:
-                raise ValueError(
-                    f"expected {target_objective} but mosek "
-                    f"primal objective = {res['primal_objective']} and "
-                    f"dual objective = {res['dual_objective']}"
-                )
+        # target_objective = problem_data.get("target_objective", None)
+        # test = (
+        #     (target_objective is not None)
+        #     and np.isfinite(res["primal_objective"])
+        #     and np.isfinite(res["dual_objective"])
+        # )
+        # if test:
+        #     target_tol = 5e-2
+        #     test = True
+        #     try:
+        #         np.testing.assert_allclose(
+        #             res["primal_objective"],
+        #             target_objective,
+        #             rtol=target_tol,
+        #             atol=target_tol,
+        #         )
+        #     except AssertionError as e:
+        #         print(e)
+        #         test = False
+        #     try:
+        #         np.testing.assert_allclose(
+        #             res["dual_objective"],
+        #             target_objective,
+        #             rtol=target_tol,
+        #             atol=target_tol,
+        #         )
+        #     except AssertionError as e:
+        #         print(e)
+        #         test = False
+        #     if not test:
+        #         raise ValueError(
+        #             f"expected {target_objective} but mosek "
+        #             f"primal objective = {res['primal_objective']} and "
+        #             f"dual objective = {res['dual_objective']}"
+        #         )
 
         return res
 
