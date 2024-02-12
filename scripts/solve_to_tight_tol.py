cd065c9d601e83390bdc6f9d23fa53e76826fa7a
Nagisa Sugishita <nagisa.sugishita@hotmail.co.jp>
2024-02-12 14:08:27 -0500

    Scan a given directory until exhausted

--- scripts/solve_to_tight_tol.py
+++ scripts/solve_to_tight_tol.py
@@ -23,31 +23,40 @@ from cpsdppy import sdpa
 from cpsdppy.sdp_solvers import mosek
 
 
-def run(data_file_path):
-    config = config_module.Config()
-    config.tol = 1e-6
-    config.feas_tol = 1e-6
-
-    output_file_path = os.path.splitext(data_file_path)[0] + ".txt"
-
-    if os.path.exists(output_file_path):
-        print(f"already exists: {output_file_path}")
-        return
-
-    print(f"solving: {data_file_path}")
-    with open(data_file_path, "r") as f:
-        problem_data = sdpa.read("".join(f.readlines()).strip())
-    res = mosek.run(problem_data, config)
-    optimal_objective = 0.5 * (res["primal_objective"] + res["dual_objective"])
-    print(f"optimal objective: {optimal_objective}")
-    with open(output_file_path, "w") as f:
-        f.write(str(optimal_objective))
+def run(data_dir):
+    updated = False
+    for file_name in sorted(os.listdir(data_dir)):
+        if not file_name.endswith(".dat-s"):
+            continue
+        data_file_path = os.path.join(data_dir, file_name)
+        output_file_path = os.path.splitext(data_file_path)[0] + ".txt"
+        if os.path.exists(output_file_path):
+            continue
+        print(f"solving: {data_file_path}")
+        config = config_module.Config()
+        config.tol = 1e-6
+        config.feas_tol = 1e-6
+        with open(data_file_path, "r") as f:
+            problem_data = sdpa.read("".join(f.readlines()).strip())
+        res = mosek.run(problem_data, config)
+        optimal_objective = 0.5 * (res["primal_objective"] + res["dual_objective"])
+        print(f"optimal objective: {optimal_objective}")
+        with open(output_file_path, "w") as f:
+            f.write(str(optimal_objective))
+        updated = True
+    return updated
 
 
 def main():
     """Run the main routine of this script"""
-    for data_file_path in sys.argv[1:]:
-        run(data_file_path)
+    while True:
+        updated = False
+        for data_dir in sys.argv[1:]:
+            print(f"scanning {data_dir}")
+            updated = run(data_dir) or updated
+        if not updated:
+            print("new files not found")
+            break
 
 
 if __name__ == "__main__":
