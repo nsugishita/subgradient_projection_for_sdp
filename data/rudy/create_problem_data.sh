de0816c34fd2a5cff2beff44031d0f624660d85b
Nagisa Sugishita <nagisa.sugishita@hotmail.co.jp>
2024-03-04 11:53:55 -0500

    Update rudy test instance file names

--- data/rudy/create_problem_data.sh
+++ data/rudy/create_problem_data.sh
@@ -11,7 +11,7 @@ mkdir -p out
 for density in 5 10 15 20 ; do
     for size in 1000 2000 3000 4000 5000 ; do
         for seed in 1 2 3 4; do
-            python data/rudy/create_max_cut.py $size $density $seed > data/rudy/out/graph_${size}_${density}_${seed}.dat-s
+            python data/rudy/create_mcp.py $size $density $seed > data/rudy/out/mcp_${size}_${density}_${seed}.dat-s
         done
     done
 done
