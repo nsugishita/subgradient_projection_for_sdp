4878ca22da6319e58dc05be15d02a7144593b335
Nagisa Sugishita <nagisa.sugishita@hotmail.co.jp>
2024-06-20 10:27:00 -0400

    Analyse bundle methods

--- scripts/analyse.py
+++ scripts/analyse.py
@@ -124,39 +124,41 @@ def emphasize(text, mode=True):
 
 def main():
     """Run the main routine of this script"""
-    for item in ["walltime", "n_iterations"]:
-        print(f"item: {item}")
-        sep = "&"
-        print(f"{'problem':>8s} ", end="")
-        for tol_i, tol in enumerate([1e-2, 1e-3]):
-            for name in ["min", "comb", "both"]:
-                print(f" {sep} ", end="")
-                print(emphasize(f"{name:>6s}", 0) + " ", end="")
-        print("\\\\")
-        for input_file_path in input_file_paths1:
-            problem_name = os.path.splitext(os.path.basename(input_file_path))[0]
-            print(f"{problem_name:8s} ", end="")
-            for tol_i, tol in enumerate([1e-2, 1e-3]):
-                values = []
-                for n_linear_cuts, eigen_comb_cut in [(1, 0), (0, 1), (1, 1)]:
-                    output_file_path = f"outputs/v3/subgradient_projection/{problem_name}_tol_{tol}_comb_{eigen_comb_cut}_linear_{n_linear_cuts}.pkl"
-                    with open(output_file_path, "rb") as f:
-                        res = pickle.load(f)
-                    values.append(res[item])
-                min_ = min([np.round(x, 1) for x in values])
-                if item == "walltime":
-                    values = [emphasize(f"{x:6.1f}", np.round(x, 1) == min_) for x in values]
-                elif item == "n_iterations":
-                    values = [emphasize(f"{x:6d}", np.round(x, 1) == min_) for x in values]
-                for x in values:
-                    print(f" {sep} ", end="")
-                    print(f"{x} ", end="")
-            print("\\\\")
+    sep = "&"
+
+    # for item in ["walltime", "n_iterations"]:
+    #     print(f"item: {item}")
+    #     print(f"{'problem':>8s} ", end="")
+    #     for tol_i, tol in enumerate([1e-2, 1e-3]):
+    #         for name in ["min", "comb", "both"]:
+    #             print(f" {sep} ", end="")
+    #             print(emphasize(f"{name:>6s}", 0) + " ", end="")
+    #     print("\\\\")
+    #     for input_file_path in input_file_paths1:
+    #         problem_name = os.path.splitext(os.path.basename(input_file_path))[0]
+    #         print(f"{problem_name:8s} ", end="")
+    #         for tol_i, tol in enumerate([1e-2, 1e-3]):
+    #             values = []
+    #             for n_linear_cuts, eigen_comb_cut in [(1, 0), (0, 1), (1, 1)]:
+    #                 output_file_path = f"outputs/v3/subgradient_projection/{problem_name}_tol_{tol}_comb_{eigen_comb_cut}_linear_{n_linear_cuts}.pkl"
+    #                 with open(output_file_path, "rb") as f:
+    #                     res = pickle.load(f)
+    #                 values.append(res[item])
+    #             min_ = min([np.round(x, 1) for x in values])
+    #             if item == "walltime":
+    #                 values = [emphasize(f"{x:6.1f}", np.round(x, 1) == min_) for x in values]
+    #             elif item == "n_iterations":
+    #                 values = [emphasize(f"{x:6d}", np.round(x, 1) == min_) for x in values]
+    #             for x in values:
+    #                 print(f" {sep} ", end="")
+    #                 print(f"{x} ", end="")
+    #         print("\\\\")
 
     for item in ["walltime", "n_iterations"]:
         print(f"{'problem':>24s} ", end="")
         for tol in [1e-2, 1e-3]:
-            for solver in ["sbgrd", "mosek", "cosmo", "cosmon", "sdpnal"]:
+            for solver in ["sbgrd", "bundle", "mosek", "cosmo", "cosmon", "sdpnal"]:
+            # for solver in ["sbgrd", "mosek", "bundle"]:
                 if solver == "cosmon":
                     continue
                 print(f" {sep} ", end="")
@@ -167,7 +169,8 @@ def main():
             print(f"{problem_name:>24s} ", end="")
             for tol in [1e-2, 1e-3]:
                 values = []
-                for solver in ["subgradient_projection", "mosek", "cosmo", "cosmon", "sdpnal"]:
+                for solver in ["subgradient_projection", "bundle", "mosek", "cosmo", "cosmon", "sdpnal"]:
+                # for solver in ["subgradient_projection", "mosek", "bundle"]:
                     if solver == "cosmon":
                         continue
                     v = load_walltime(input_file_path, tol, solver, item)
@@ -188,7 +191,7 @@ def main():
         print(f"{'':>8s} ", end="")
         for density in [5, 10]:
             for tol_i, tol in enumerate([1e-2, 1e-3]):
-                for solver_i, solver in enumerate(["sbgrd", "mosek"]):
+                for solver_i, solver in enumerate(["sbgrd", "bundle", "mosek"]):
                     print(f" {sep} ", end="")
                     if tol_i == 0 and solver_i == 0:
                         print(emphasize(f"d: {density:3d}", 0) + " ", end="")
@@ -202,7 +205,7 @@ def main():
         print(f"{'instance':>8s} ", end="")
         for density in [5, 10]:
             for tol in [1e-2, 1e-3]:
-                for solver in ["sbgrd", "mosek"]:
+                for solver_i, solver in enumerate(["sbgrd", "bundle", "mosek"]):
                     print(f" {sep} ", end="")
                     print(emphasize(f"{solver:>6s}", 0) + " ", end="")
         print("\\\\")
@@ -218,7 +221,7 @@ def main():
                     input_file_path = f"data/rudy/out/{problem_name}_{size}_{density}_{random_seed}.dat-s"
                     for tol in [1e-2, 1e-3]:
                         values = []
-                        for solver in ["subgradient_projection", "mosek"]:
+                        for solver in ["subgradient_projection", "bundle", "mosek"]:
                             values.append(load_walltime(input_file_path, tol, solver))
                         min_ = min([np.round(x, 1) for x in values])
                         values = [emphasize(f"{x:6.1f}", np.round(x, 1) == min_) for x in values]
@@ -254,6 +257,16 @@ def load_walltime(input_file_path, tol, solver, item="walltime"):
         output_file_path = f"outputs/v3/sdpnal/{problem_name}_{tol}_0.001.npz"
         res = np.load(output_file_path)
         return res[item][-1]
+    if solver == "bundle":
+        try:
+            problem_name = os.path.splitext(os.path.basename(input_file_path))[0]
+            output_file_path = f"outputs/v3/ConicBundle/tol_{tol}_{problem_name}.txt"
+            with open(output_file_path, "r") as f:
+                res = yaml.safe_load(f)
+            return res[item]
+        except FileNotFoundError:
+            return np.nan
+    raise ValueError(solver)
 
 
 if __name__ == "__main__":
